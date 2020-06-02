2ddac9b7754f37cc8dba63f97d78d60c15db6ef8
codingcyber <vivek@codingcyber.com>
2020-06-02 11:22:48 +0530

    2-7 Optimising Strategy for Bullish & Bearish Candles

--- strategies/20ma-algo.php
+++ strategies/20ma-algo.php
@@ -24,32 +24,44 @@ foreach ($stockvals as $stockval) {
   // if the price_close is above 20 sma line(price_close greater than 20 sma value), it's buy signal
   // if previous value is not empty, we will check the current candle & previous candle. If the previous candle is empty, we will check only current candle
   // for BUY signal, we need only bullish candles (that is green candles)
+  // for SELL signal, we need only bearish candles (that is red candle)
   if(!empty($previousval)){
+    // how we can find the bullish & bearish candles?
+    // for bullish candles open price is below the closing price, that forms the green candle (price close greater than price open) or (price open less than price close)
+    // for bearish candles open price is above the closing price, that forms the red candle (price close less than price open) or (price open greater than price close)
     if(($previousval['price_close'] < $previousval['20sma']) && ($stockval['price_close'] > $stockval['20sma'])){
-      echo "<p style='color:green;'>";
-      echo "BUY " . $stockval['trade_date'] . " - " . $stockval['price_close'] . " - 1<br>";
-      echo "</p>";
+      if($stockval['price_close'] > $stockval['price_open']){
+        echo "<p style='color:green;'>";
+        echo "BUY " . $stockval['trade_date'] . " - " . $stockval['price_close'] . " - 1<br>";
+        echo "</p>";
+      }
     }
   }else{
     if($stockval['price_close'] > $stockval['20sma']){
-      echo "<p style='color:green;'>";
-      echo "BUY " . $stockval['trade_date'] . " - " . $stockval['price_close'] . " - 2<br>";
-      echo "</p>";
+      if($stockval['price_close'] > $stockval['price_open']){
+        echo "<p style='color:green;'>";
+        echo "BUY " . $stockval['trade_date'] . " - " . $stockval['price_close'] . " - 2<br>";
+        echo "</p>";
+      }
     }
   }
   // SELL signal
   // if the price_close is below 20 sma line(price_close less than 20 sma value), it's sell signal
   if(!empty($previousval)){
     if(($previousval['price_close'] > $previousval['20sma']) && ($stockval['price_close'] < $stockval['20sma'])){
-      echo "<p style='color:red;'>";
-      echo "SELL " . $stockval['trade_date'] . " - " . $stockval['price_close'] . " - 3<br>";
-      echo "</p>";
+      if($stockval['price_close'] < $stockval['price_open']){
+        echo "<p style='color:red;'>";
+        echo "SELL " . $stockval['trade_date'] . " - " . $stockval['price_close'] . " - 3<br>";
+        echo "</p>";
+      }
     }
   }else{
     if($stockval['price_close'] < $stockval['20sma']){
-      echo "<p style='color:red;'>";
-      echo "SELL " . $stockval['trade_date'] . " - " . $stockval['price_close'] . " - 4<br>";
-      echo "</p>";
+      if($stockval['price_close'] < $stockval['price_open']){
+        echo "<p style='color:red;'>";
+        echo "SELL " . $stockval['trade_date'] . " - " . $stockval['price_close'] . " - 4<br>";
+        echo "</p>";
+      }
     }
   }
   $previousval = $stockval;
